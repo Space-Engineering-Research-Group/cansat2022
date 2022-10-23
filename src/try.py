ff4271dab1aa938bfe961965b3998c50865475d1
emp-temp <emptemp0000@gmail.com>
2022-10-23 16:12:33 +0900

    change modules

--- src/try.py
+++ src/try.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import modules
 
 for i in range(10):
@@ -33,4 +34,23 @@ md.DCmotor.right_role(duty , t)
 
 md.cds.read()
 
+=======
+import modules as md
+
+for i in range(10):
+    data = str(md.GPS.Read_data)
+    print(data)
+
+md.servo.role(90)
+
+t = 1000
+duty = 0.5
+md.DCmotor.foword(duty , t)
+md.DCmotor.back(duty , t)
+md.DCmotor.left_role(duty , t)
+md.DCmotor.right_role(duty , t)
+
+md.cds.read()
+
+>>>>>>> 9dcdc1a (change modules)
 md.xbee_send.send_message(DATA_TO_SEND="Hello Xbee!")
