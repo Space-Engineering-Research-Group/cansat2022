189abd0c017700d87634859fda34068964385ebd
Nusk-Rbb <github>
2022-11-10 18:06:05 +0900

    ２つの角度から距離を計算する関数を追加しました

--- src/main.py
+++ src/main.py
@@ -7,15 +7,26 @@ GPIO.setup(25, GPIO.IN)
 
 CDS_PIN = 25
 
+Rp = 6356.8 * 1000#[m] (地球の極半径)
+Re = 6378.1 * 1000#[m] (地球の赤道半径)
+pi = 3.141592
+
 def main():
     #TODO: 落下してからパラシュートを切り離すまでの動作
     while GPIO.input(CDS_PIN) >= 100:   #TODO: CDSが読み取る値　要検証
             deployment(90)
             break
 
+    gps_start = [gps()]
+    gps_x0 = gps_start[-1]
+    gps_y0 = gps_start[-2]
     #TODO: ゴールするまでの動作
-    while 1:
-        gps_data = gps()
+    while calc_moment(x = 2*pi*Re*(gps_x0 / 360) , y = 2*pi*Rp*(gps_y0 / 360) , data='d') < 10:
+        move_motor(direc='fd' , duty=1.0 , time=1000)
+        temp_gps = gps()
+        xbee(temp_gps)
+        
+         
 
 # --------------------------------------------
 
@@ -25,12 +36,17 @@ def deployment(angle):
     return modules.servo.role(angle)
 
 #TODO: 2つの座補から角度と距離を返す関数
-def calc_moment(x0 , y0 , x , y):
+def calc_moment(x0 , y0 , x=modules.GPS.GPS_xonly() , y=modules.GPS.GPS_yonly() , data='a'):
     distance_x = x - x0
     distance_y = y - y0
     distance = math.sqrt(distance_x**2 + distance_y**2)
     sita = math.atan(distance_y/distance_x)
-    return float(distance) , float(sita)
+    if data == 'd':
+        return float(distance)
+    elif data == 's':
+        return float(sita)
+    else:
+        return float(distance) , float(sita)
 
 #TODO: モーターを動かすコードを書く
 def move_motor(direc , duty , time):
@@ -47,7 +63,18 @@ def move_motor(direc , duty , time):
 def gps():
     return modules.GPS.Read_data()
 
+#TODO: xbeeでデータを送る関数
+def xbee(s):
+    return modules.xbee_send(s)
 
+def goals_data():
+    xbee("Please stand at goal")
+    while 1:
+        try:
+            gps_data = gps()
+            xbee(gps_data)
+        except KeyboardInterrupt:
+            return gps()
 
 if __name__ == "__main__":
     main()
