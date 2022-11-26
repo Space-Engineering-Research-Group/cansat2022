6e0277b92c8226495ca101d980c0bfec9b58fb70
Nusk-Rbb <github>
2022-11-27 03:12:49 +0900

    コメント追加しました

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import math
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
 GPIO.setup(25, GPIO.IN)
 
-CDS_PIN = 25
+CDS_PIN = 25               # CDSのピン番号
 
 Rp = 6356.8 * 1000#[m] (地球の極半径)
 Re = 6378.1 * 1000#[m] (地球の赤道半径)
@@ -50,21 +50,23 @@ def main():
 
 
 #TODO: サーボを使って展開するコードを書く
-def deployment(angle):
+def deployment(angle):  #angle[°]だけサーボが回転する
     return modules.servo.role(angle)
 
 #TODO: 2つの座補から角度と距離を返す関数
 def calc_moment(
-    x0 = modules.GPS.GPS_xonly(gps_data=gps_x0),
+    x0 = modules.GPS.GPS_xonly(gps_data=gps_x0),    #GPSから取得した座標を読み取る
     y0 = modules.GPS.GPS_yonly(gps_data=gps_y0),
-    x = modules.GPS.GPS_xonly(),
+    x = modules.GPS.GPS_xonly(),                    #ゴールの座標
     y = modules.GPS.GPS_yonly(),
     data='a'
     ):
+    #ゴールの方向を計算する
     distance_x = x - x0
     distance_y = y - y0
     distance = math.sqrt(distance_x**2 + distance_y**2)
     sita = math.atan(distance_y/distance_x)
+    #dで距離、sで角度、初期値(a)で両方出力する
     if data == 'd':
         return float(distance)#[m]
     elif data == 's':
@@ -74,13 +76,17 @@ def calc_moment(
 
 #TODO: モーターを動かすコードを書く
 def move_motor(direc , duty , time):
-    if direc == 'fd':
+    #f -> 前進
+    if direc == 'f':
         return modules.DCmotor.foword(duty , time)
-    elif direc == 'bc':
+    #b -> 後進
+    elif direc == 'b':
         return modules.DCmotor.back(duty , time)
-    elif direc == 'rt':
+    #r -> 右回転
+    elif direc == 'r':
         return modules.DCmotor.right_role(duty , time)
-    elif direc == 'lt':
+    #l -> 左回転
+    elif direc == 'l':
         return modules.DCmotor.left_role(duty ,time) 
 
 #TODO: GPSを読み取る関数
@@ -94,9 +100,12 @@ def gps(option = 'all'):
 
 #TODO: xbeeでデータを送る関数
 def xbee(s):
+    #いちいち打つのめんどい
     return modules.xbee_send(s)
 
+#TODO: GPSを使ってゴール座標を取ってくる
 def goals_data():
+
     xbee("Please stand at goal")
     while 1:
         try:
